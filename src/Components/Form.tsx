7c3460c9c3439c1a7c09d7deefec2b2869ea41e2
SergeyRO <sergey_r.o@mail.ru>
2023-10-23 22:00:24 +0300

    first

--- src/Components/Form.tsx
+++ src/Components/Form.tsx
@@ -1,21 +1,30 @@
 import React from 'react'
 import TableSteps from './TableSteps'
 
+export interface IForm{
+    data: string,
+    steps: number
+}
+
 export default function Form() {
-    const handleSubmit = (e: React.FormEventHandler<>) => {
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) =>{
+        console.log(e.target.value)
+    }
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-        console.log('click')
+        console.log(e.target)
+    
     }        
   return (
     <>
     <form onSubmit={handleSubmit}>
         <div style={{width:'35%', float:'left', marginRight:'10px',paddingBottom:'10px'}}>
             <label htmlFor='DateSteps'>Дата (ДД.ММ.ГГ)</label><br/>
-            <input name='DateSteps' id='DateSteps' type='date'/>
+            <input name='DateSteps' id='DateSteps' type='date' onChange={handleChange}/>
         </div>
         <div style={{width:'47%', float:'left', marginRight:'5px'}}>
             <label htmlFor='Steps'>Пройдено км</label><br/>
-            <input name='Steps' id='Steps'/>
+            <input name='Steps' id='Steps' onChange={handleChange}/>
         </div>
         <div style={{width:'0%', float:'left', marginLeft:'15px'}}>
             <br />
