f4420385691fdab79155d50e46ba024a75a9ebff
sergeyRO <sergey_r.o@mail.ru>
2023-10-24 17:15:38 +0300

    first

--- src/Components/Form.tsx
+++ src/Components/Form.tsx
@@ -1,30 +1,51 @@
-import React from 'react'
+import React, {useState} from 'react'
 import TableSteps from './TableSteps'
 
-export interface IForm{
+export interface IForm {
     data: string,
     steps: number
 }
 
+type TTable = Array<IForm>
+
 export default function Form() {
+    const [form, SetForm] = useState<IForm>(
+        {
+            data: "",
+            steps: 0
+        }
+    )
+    const [table, SetTable] = useState<TTable>([])
+
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) =>{
         console.log(e.target.value)
+        console.log(e.target.name)
+        SetForm(prevForm => ({...prevForm, [e.target.name]: e.target.value}))
     }
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-        console.log(e.target)
-    
+        console.log(form)
+        table.push({
+            form
+        })
+        console.log(table)
+      /* SetTable(prevTable => ({...prevTable,
+        push({
+            data: form.data,
+            steps: form.steps
+    })}))*/
+        
     }        
   return (
     <>
     <form onSubmit={handleSubmit}>
         <div style={{width:'35%', float:'left', marginRight:'10px',paddingBottom:'10px'}}>
-            <label htmlFor='DateSteps'>Дата (ДД.ММ.ГГ)</label><br/>
-            <input name='DateSteps' id='DateSteps' type='date' onChange={handleChange}/>
+            <label htmlFor='data'>Дата (ДД.ММ.ГГ)</label><br/>
+            <input name='data' id='data' type='date' onChange={handleChange}/>
         </div>
         <div style={{width:'47%', float:'left', marginRight:'5px'}}>
-            <label htmlFor='Steps'>Пройдено км</label><br/>
-            <input name='Steps' id='Steps' onChange={handleChange}/>
+            <label htmlFor='steps'>Пройдено км</label><br/>
+            <input name='steps' id='steps' onChange={handleChange}/>
         </div>
         <div style={{width:'0%', float:'left', marginLeft:'15px'}}>
             <br />
--- src/Components/TableSteps.tsx
+++ src/Components/TableSteps.tsx
@@ -1,19 +1,20 @@
-import React from 'react'
+import React from 'react';
+
 
 export default function TableSteps() {
-  return (
-    <>
-        <div style={{border:'1px solid black', borderRadius:'2px',display: 'flex',height: '100%',fontSize:'10px'}}>
-            <div style={{width:'40%', float:'left', marginRight:'10px'}}>
-                <label>Дата (ДД.ММ.ГГ)</label>
+    return (
+        <>
+            <div style={{ border: '1px solid black', borderRadius: '2px', display: 'flex', height: '100%', fontSize: '10px' }}>
+                <div style={{ width: '40%', float: 'left', marginRight: '10px' }}>
+                    <label>Дата (ДД.ММ.ГГ)</label>
+                </div>
+                <div style={{ width: '40%', float: 'left', marginRight: '5px' }}>
+                    <label>Пройдено км</label>
+                </div>
+                <div style={{ width: '20%', float: 'left', marginLeft: '5px' }}>
+                    <label>Действия</label>
+                </div>
             </div>
-            <div style={{width:'40%', float:'left', marginRight:'5px'}}>
-                <label>Пройдено км</label>
-            </div>
-            <div style={{width:'20%', float:'left', marginLeft:'5px'}}>
-                <label>Действия</label>
-            </div>
-        </div>
-    </>
-  )
+        </>
+    );
 }
